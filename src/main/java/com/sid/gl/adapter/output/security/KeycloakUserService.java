69ef1f1025e15808a78067d9c4cebc07af42ef96
Adama SEYE <a.seye3777@zig.univ.sn>
2025-08-08 15:52:34 +0200

    review keycloak

--- src/main/java/com/sid/gl/adapter/output/security/KeycloakUserService.java
+++ src/main/java/com/sid/gl/adapter/output/security/KeycloakUserService.java
@@ -5,6 +5,7 @@ import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,26 +14,26 @@ import java.util.List;
 @Service
 @Slf4j
 public class KeycloakUserService {
-    @Value("${keycloak.admin.server-url}")
-    private String serverUrl;
-    @Value("${keycloak.admin.realm}")
-    private String realm;
-    @Value("${keycloak.admin.username}")
-    private String adminUsername;
-    @Value("${keycloak.admin.password}")
-    private String adminPassword;
-    @Value("${keycloak.admin.client-id}")
-    private String clientId;
+
+    private final KeyCloakProperties properties;
+
+    public KeycloakUserService(KeyCloakProperties properties) {
+        this.properties = properties;
+    }
 
     public void createUserCredKeycloak(String name,String email,String password){
         log.info("process to create user in keycloak with username: {}, email: {}, password: {}", name, email, password);
         var kc = KeycloakBuilder.builder()
-                .serverUrl(serverUrl)
-                .realm(realm)
-                .grantType(OAuth2Constants.PASSWORD)
+                .serverUrl(properties.getServerUrl())
+                .realm(properties.getRealm())
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .clientId(properties.getClientId())
+                .clientSecret(properties.getClientSecret())
+                .build();
+                /*.grantType(OAuth2Constants.PASSWORD)
                 .username(adminUsername).password(adminPassword)
                 .clientId(clientId)
-                .build();
+                .build();*/
 
         var kcUser = new UserRepresentation();
         kcUser.setUsername(email);
@@ -45,7 +46,7 @@ public class KeycloakUserService {
         cred.setValue(password);
         kcUser.setCredentials(List.of(cred));
 
-        var resp = kc.realm(realm).users().create(kcUser);
+        var resp = kc.realm(properties.getRealm()).users().create(kcUser);
         if(resp.getStatus() != 201) {
             log.error("Error creating user in Keycloak: {}", resp.getStatusInfo());
             throw new RuntimeException("Failed to create user in Keycloak");
